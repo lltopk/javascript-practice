3fb2a287f0171d6de2fab09c34b2c3039eb283f2
lltopk <liuyanoutsee@outlook.com>
2025-11-13 19:04:41 +0800

    feat syntax spread

--- src/syntax_spread/test.js
+++ src/syntax_spread/test.js
@@ -4,7 +4,15 @@ let arr = {
     c:3
 }
 
+//当没有定义字段d, 或者从后端返回的字段中没有d的时候(对于后端没查到返回null, 但是对于前端是undefined)
 let newArr = {
+    ...arr,
+    d: arr.d
+}
+console.log(newArr)
+
+//添加或运算符, 防止undefined, 避免后期传给后端接口报错, 传了字符串'undefined'
+newArr = {
     ...arr,
     d: arr.d || 4
 }
