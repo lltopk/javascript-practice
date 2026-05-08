eeaddf3e6c008740f6530c7fd57d46cf10ccfc65
lltopk <liuyanoutsee@outlook.com>
2026-05-08 21:14:35 +0800

    update

--- src/promise/00.promise_syntax.js
+++ src/promise/00.promise_syntax.js
@@ -30,13 +30,10 @@ let promise3 = new Promise((resolve, reject) =>{
 );
 
 // resolve runs the first function in .then
-// 重要区分: 
+// 重要区分:
 // 如果说定义promise的resolve函数和reject函数是JavaScript引擎提供的, 则"真实用户(回调函数提供者)"是JavaScript本身
 // 那么在调用promise的then方法时, 传入的回调函数result和error则是我们自己事先提供的.则"真实用户(回调函数提供者)"是我们自己
 promise3.then(
-    function(result) { console.log(result); }, // shows "done!" after 1 second
-    function(error) { console.log(error); }   // doesn't run
+  function(result) { console.log("success",result); }, // shows "done!" after 1 second
+    function(error) { console.log("error",error); }   // doesn't run
 );
-
-
-
